15146f7740e329a7b2c8e16b84e5cdc186a480cd
jjjy879 <jjjy879@naver.com>
2022-07-28 14:17:58 +0900

    .

--- mysql/sql/basic.sql
+++ mysql/sql/basic.sql
@@ -1,2 +1,21 @@
 -- milktea database를 사용하겠다.
-use milktea;
+USE milktea;
+
+-- 중요 *** 테이블 수정 전에 항상 꼭 반드시 진짜로 ER부터 수정
+
+-- 전체 컬럼 조회
+SELECT * FROM member2;
+
+-- 컬럼 추가
+ALTER TABLE member2 ADD COLUMN nickname VARCHAR(45);
+ALTER TABLE member2 ADD COLUMN name VARCHAR(45);
+ALTER TABLE member2 ADD COLUMN nameEng VARCHAR(45) AFTER name;
+ALTER TABLE member2 ADD COLUMN id VARCHAR(45);
+ALTER TABLE member2 ADD COLUMN dob INT;
+
+-- 컬럼 변경
+ALTER TABLE member2 MODIFY COLUMN nickname VARCHAR(100);
+
+-- 컬럼 이름 변경
+ALTER TABLE member2 CHANGE COLUMN nickname nick VARCHAR(45);
+ALTER TABLE member2 CHANGE COLUMN nameEng nameKor VARCHAR(45);
