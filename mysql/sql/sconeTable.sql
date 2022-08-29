dda43bc0d7bd19b261d9653e118100c9f69b5e04
jjjy879 <jjjy879@naver.com>
2022-08-30 01:18:25 +0900

    .

---
+++ mysql/sql/sconeTable.sql
@@ -0,0 +1,37 @@
+use scone;
+
+SELECT *
+FROM member;
+
+SELECT *
+FROM item;
+
+SELECT *
+FROM itemoption;
+
+SELECT *
+FROM qna;
+
+SELECT *
+FROM review;
+
+SELECT *
+FROM mypage;
+
+SELECT *
+FROM payment;
+
+SELECT *
+FROM buy_item;
+
+SELECT *
+FROM orderlist;
+
+SELECT *
+FROM sellerinfo;
+
+SELECT *
+FROM CodeGroup;
+
+SELECT *
+FROM Code;
